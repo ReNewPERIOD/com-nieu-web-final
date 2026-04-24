fd66ba3be686432267ad78e48ddd9064c897e474
ReNewPERIOD <ngotranthanhhuy83@gmail.com>
2026-04-24 07:46:37 +0700

    Remove trailingSlash option from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "dev-comnieudanhtien.pantheonsite.io",
-        port: "",
-        pathname: "/**",
       },
     ],
   },
