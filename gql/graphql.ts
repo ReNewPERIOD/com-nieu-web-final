8290889bfc6ddd3fdb9613d13d378b7c522d6aa3
ReNewPERIOD <ngotranthanhhuy83@gmail.com>
2026-04-25 10:27:08 +0700

    Delete gql directory

--- gql/graphql.ts
+++
@@ -1,4 +0,0 @@
-export const graphql = (query: string) => query;
-export function fragmentMasking() { return null; }
-export function makeFragmentData() { return null; }
-export function useFragment() { return null; }
