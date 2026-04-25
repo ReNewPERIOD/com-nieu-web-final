f07b705afddee81f4810ee7d373ef214822d3994
ReNewPERIOD <ngotranthanhhuy83@gmail.com>
2026-04-25 10:30:42 +0700

    Update output paths for GraphQL code generation

--- codegen.ts
+++ codegen.ts
@@ -5,14 +5,12 @@ const config: CodegenConfig = {
   // Dùng link chuẩn, Vercel sẽ tự xử lý tốt hơn máy cá nhân
   schema: "https://dev-comnieudanhtien.pantheonsite.io/graphql", 
   generates: {
-    "gql/": {
+    "src/gql/": {
       preset: "client",
-      plugins: [],
     },
-    "gql/schema.gql": {
+    "src/gql/schema.gql": {
       plugins: ["schema-ast"],
     },
   },
-};
 
 export default config;
