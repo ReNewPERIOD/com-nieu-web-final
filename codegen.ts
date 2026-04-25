8cbbc57849d65527de1a180dd1fbad75b8a363cd
ReNewPERIOD <ngotranthanhhuy83@gmail.com>
2026-04-25 10:01:53 +0700

    Update GraphQL schema URL in codegen configuration

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,13 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
-import { loadEnvConfig } from "@next/env";
-
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
 
 const config: CodegenConfig = {
   overwrite: true,
-  // Dùng link trực tiếp có dấu ? để ổn định nhất trên Pantheon
-  schema: "https://dev-comnieudanhtien.pantheonsite.io/?graphql", 
+  // Dùng link chuẩn, Vercel sẽ tự xử lý tốt hơn máy cá nhân
+  schema: "https://dev-comnieudanhtien.pantheonsite.io/graphql", 
   generates: {
-    "gql/": {  // Bỏ chữ src/ đi vì project của bạn không dùng thư mục src
+    "gql/": {
       preset: "client",
+      plugins: [],
     },
     "gql/schema.gql": {
       plugins: ["schema-ast"],
