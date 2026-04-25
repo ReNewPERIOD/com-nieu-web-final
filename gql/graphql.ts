46e263568452547440bee57869d8a72445401ad8
ReNewPERIOD <ngotranthanhhuy83@gmail.com>
2026-04-25 10:19:37 +0700

    Add fragment functions for GraphQL handling

--- gql/graphql.ts
+++ gql/graphql.ts
@@ -1,1 +1,4 @@
 export const graphql = (query: string) => query;
+export function fragmentMasking() { return null; }
+export function makeFragmentData() { return null; }
+export function useFragment() { return null; }
